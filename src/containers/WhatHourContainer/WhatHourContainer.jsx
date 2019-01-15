d70c70ba245efbea502574d532516f5c5a1fecd4
Kasper Green <kaspergreen@gmail.com>
2019-01-16 00:24:39 +0300

    what hour update interval

--- src/containers/WhatHourContainer/WhatHourContainer.jsx
+++ src/containers/WhatHourContainer/WhatHourContainer.jsx
@@ -4,14 +4,28 @@ import { slide_left } from 'containers/App/transitions'
 import BackButton from 'components/BackButton/BackButton'
 
 export default class WhatHourContainer extends Component {
+
   render () {
     return (
       <Fragment>
         <BackButton invert to_right to={{pathname: '/', state: slide_left}} />
         <div className='WhatHour'>
-          Сейчас: {Math.ceil(new Date().getTime() / 3600000).toString().replace(/(.{3})/, '$1 ')} час
+          Сейчас: {String(Math.ceil(new Date().getTime() / 3600000)).replace(/(.{3})/, '$1 ')} час
         </div>
       </Fragment>
     )
   }
+
+  update_component_interval
+
+  componentDidMount () {
+    this.update_component_interval = setInterval(() => {
+      this.forceUpdate()
+    }, 10000)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.update_component_interval)
+  }
+
 }
